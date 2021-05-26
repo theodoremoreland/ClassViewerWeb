e2d1fa308c816b10558a38f907917e3d4d67beb8
Theo Moreland <theodore.moreland.ext@bayer.com>
2021-05-26 18:04:02 -0500

    Updated boilerplate and added Card component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
+
+
+// Components
+import Card from './components/Card';
+
+// Styles
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card title="Unit 2 - Java" subtitle="Lc101 2021"/>
+    </>
   );
 }
 
