0e230ae91d137d83db0eebd70f2662b485c95f49
Theo Moreland <theodore.moreland.ext@bayer.com>
2021-05-27 13:00:47 -0500

    Updated App to include Panel and improved CSS

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 
 // Components
+import Panel from './components/Panel';
 import Card from './components/Card';
+
+// Icons
+import { GiCalendar, GiMailbox, GiHood } from "react-icons/gi"; 
 
 // Styles
 import './App.css';
@@ -10,7 +14,15 @@ import './App.css';
 function App() {
   return (
     <>
-      <Card title="Unit 2 - Java" subtitle="Lc101 2021"/>
+      <div id="controlPanelContainer">
+        <Panel title="Class Viewer Web" icons={[ GiHood, GiCalendar, GiMailbox,]} />
+      </div>
+      <div id="contentContainer">
+        <Card title="Unit 1 - JavaScript" subtitle="Lc101 2021"/>
+        <Card title="Unit 2 - Java" subtitle="Lc101 2021"/>
+        <Card title="Unit 1 - Python" subtitle="Lc101 2018"/>
+        <Card title="Unit 2 - Java" subtitle="Lc101 2018"/>
+      </div>
     </>
   );
 }
