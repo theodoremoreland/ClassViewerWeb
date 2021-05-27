a1cb7ec8b39e455258c1293d2c98358a236061cd
Theo Moreland <theodore.moreland.ext@bayer.com>
2021-05-27 13:00:17 -0500

    Added Panel component and CSS

---
+++ front-end/src/components/Panel.tsx
@@ -0,0 +1,26 @@
+// React
+import React, { FunctionComponent } from 'react';
+import { IconType } from 'react-icons';
+
+// Styles
+import './Panel.css';
+
+type PanelProps = {
+  title: string,
+  icons: IconType[]
+}
+
+const Panel: FunctionComponent<PanelProps> = ({ title, icons }) => {
+
+
+  return (
+    <div className="panel">
+        <h3 className="panelTitle">{title}</h3>
+      {
+        icons.map((Icon) => <div><Icon className="icon" size={50}/></div>)
+      }
+    </div>
+  )
+};
+
+export default Panel;
