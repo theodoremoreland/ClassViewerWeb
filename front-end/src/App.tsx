3e5f21a9efd706ca46610c7dc52a5d5b4a36af54
Theo Moreland <theodore.moreland.ext@bayer.com>
2021-05-28 16:31:59 -0500

    Added routing and abstracted initial view

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,12 @@
 // React
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // Components
 import Panel from './components/Panel';
-import Card from './components/Card';
 
-// api calls
-import getCourses from './api/getCourses';
+// Views
+import CoursesView from './views/Courses';
 
 // Icons
 import { GiCalendar
@@ -19,32 +19,20 @@ import { GiCalendar
 // Styles
 import './App.css';
 
-interface c {
-  id: number
-  type: string
-}
-
 function App() {
-  const [courses, setCourses] = useState<Array<c>>([]);
-
-  useEffect(() => {
-    getCourses("http://localhost:3001/courses")
-      .then(s => setCourses(s));
-  }, []);
-  
 
   return (
     <>
-      <div id="controlPanelContainer">
-        <Panel title="Class Viewer Web" icons={[ GiHouse, GiHood, GiCalendar, GiMailbox, GiExitDoor]} />
-      </div>
-      <div id="contentContainer">
-        {
-          courses[0] !== undefined
-            ? courses.map((course) => <Card key={course.id} title={course.type} subtitle=""/>)
-            : ""
-        }
-      </div>
+      <BrowserRouter>
+        <div id="controlPanelContainer">
+          <Panel title="Class Viewer Web" icons={[ GiHouse, GiHood, GiCalendar, GiMailbox, GiExitDoor]} />
+        </div>
+        <div id="contentContainer">
+          <Switch>
+            <Route path="/" component={CoursesView}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
